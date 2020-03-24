15535321ecbd0c449258fab762c82312663c337a
Maksym Pidlisnyi <p.maxsym@gmail.com>
2020-03-24 09:14:55 +0200

    fixed bugs

--- src/main/java/com/example/HW6/Hw6Application.java
+++ src/main/java/com/example/HW6/Hw6Application.java
@@ -2,12 +2,16 @@ package com.example.HW6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Hw6Application {
 
+    private static BookService bookService;
+
 	public static void main(String[] args) {
-		SpringApplication.run(Hw6Application.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(Hw6Application.class, args);
+        bookService = applicationContext.getBean(BookService.class);
 	}
 
 }
